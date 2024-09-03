0de614475d376a3ddf24b06346788aae0a4587dc
viniciusdelemos972 <161029306+viniciusdelemos972@users.noreply.github.com>
2024-09-03 10:45:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function calcula(opera){
     
+
+let num1= parseFloat(document.grtelementById('zeroum').value);
+let num2= parseFloat(document.grtelementById('zeroum').value);
+let resultado=0;
+
 }
