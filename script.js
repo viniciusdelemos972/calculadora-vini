cfa1a326a1b228c94d998c6938fa703dfc9ce053
viniciusdelemos972 <161029306+viniciusdelemos972@users.noreply.github.com>
2024-09-05 12:03:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ function calcula(opera){
 let num1= parseFloat(document.grtelementById('zeroum').value);
 let num2= parseFloat(document.grtelementById('zeroum').value);
 let resultado=0;
+if(isNaM(num1))|| isNaM(num2){
+    alert('acorde para a vida');
+}else{
+    if(opera=='+')}
+    resultado=
 
 }
